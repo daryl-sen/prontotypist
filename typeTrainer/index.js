685a58a1ce0353f4accc156807f75a96ac62f00d
Daryl <55649534+daryl-sen@users.noreply.github.com>
2021-02-11 22:03:51 -0800

    add checkWord function

--- typeTrainer/index.js
+++ typeTrainer/index.js
@@ -1,12 +1,49 @@
-$(document).ready(function() {
-  $('#typing-field').on('keyup', function() {
-    const newVal = 140 - $(this).val().length;
-    const counter = $(this).parent().children('#tweetButtonAndCounter').children('output');
-    counter.val(newVal);
-    if (newVal < 0) {
-      counter.addClass('red');
+const sentence = 'This is my sentence.';
+const refSentenceArr = sentence.split(' ');
+
+// cursor tracks which word is being typed
+let cursor = 0;
+let currentWord = refSentenceArr[cursor];
+
+// initial condition
+let completeWords = [];
+let incompleteWords = [];
+for (const word of refSentenceArr) {
+  incompleteWords.push(word);
+}
+
+
+// helper function - check if word is correct
+const checkWord = function(typed, reference) {
+  // add a space to both
+  typed += ' ';
+  reference += ' ';
+
+  let output = '';
+
+  // check for passed condition, terminate early if possible
+  if (typed === reference) {
+    output = `<span class="correct">${reference} </span>`;
+    return {passed: true, result: output};
+  }
+
+  // check to see if each letter is correct and return it in a specific color
+  for (let i = 0; i < reference.length; i++) {
+    if (typed[i] === reference[i]) {
+      output += `<span class="correct">${reference[i]}</span>`;
     } else {
-      counter.removeClass('red');
+      output += `<span class="incorrect">${reference[i]}</span>`;
     }
-  });
-});
+  }
+
+  return {passed: true, result: output};
+};
+
+
+// runs on key up of the typing box
+const checkProgress = function(typedEntry) => {
+  const typedWord = document.getElementById('typing-field');
+  const reference = refSentenceArr[cursor];
+  const targetWord = checkWord();
+  
+};
